b712f0145203e24ab3430b06f7298f19205a6e49
Vivek3927 <vivek.vas27@gmail.com>
2024-07-01 13:37:14 +0530

    Created class and setup RSpec

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+	let(:calculator) { StringCalculator.new }
+end
---
+++ string_calculator.rb
@@ -0,0 +1,5 @@
+# String calculator for add numbers
+
+class StringCalculator
+	def add(numbers); end
+end
