496e956555530bf1496102801265766e0c8c0f60
Vivek3927 <vivek.vas27@gmail.com>
2024-07-01 14:43:01 +0530

    Handle multiple lines between numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,12 @@
 # String calculator for add numbers
+require 'byebug'
 
 class StringCalculator
-	def add(numbers); end
+	def add(numbers)
+		return 0 if numbers.empty?
+		return numbers.to_i if numbers.length == 1
+
+		numbers.split(',').map(&:to_i).sum
+		numbers.split(/,|\n/).map(&:to_i).sum
+	end
 end
